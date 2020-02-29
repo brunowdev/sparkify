dc3681f462c13baf70f4eecfb754295964e0c746
Bruno Bitencourt Luiz <obrunobitencourt@gmail.com>
2020-02-29 17:09:30 -0300

    Move some common functions

---
+++ jupyter_utils.py
@@ -0,0 +1,23 @@
+
+def maximize_notebook():
+    try:
+        from IPython.core.display import display, HTML
+        display(HTML("<style>.container { width:100% !important; }</style>"))
+    except Exception as e:
+        print(f'Error while maximizing the notebook cells: {str(e)}')
+
+def apply_options(pd, max_rows = 500, max_columns = 500, d_width = 1000):
+    if pd is None:
+        print('Please import pandas before calling this function.')
+        return
+
+    pd.set_option('display.expand_frame_repr', True)
+    pd.set_option('display.max_rows', max_rows)
+    pd.set_option('display.max_columns', max_columns)
+    pd.set_option('display.width', d_width)
+
+def reload(module):
+    import importlib
+    importlib.reload(module)
+
+maximize_notebook()
